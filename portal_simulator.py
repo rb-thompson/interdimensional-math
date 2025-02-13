f5174dc7e6e68b70c096c2cc1513c0873ca42096
Brandon Thompson <prizecoffeecup@gmail.com>
2025-02-13 11:09:18 -0500

    Define transformation matrices

--- portal_simulator.py
+++ portal_simulator.py
@@ -3,3 +3,17 @@ import matplotlib.pyplot as plt     # visualizing plots
 
 # Define a vector (object position)
 object_position = np.array([1, 1])
+
+# Define transformation matrices
+theta = np.pi / 2                   # 'theta': a plane angle
+print(theta)                        #  output: 1.5707963267948966
+
+# Rotation matrix (90 degrees)
+rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)],
+                            [np.sin(theta), np.cos(theta)]])
+
+# Scaling matrix (double the size)
+scaling_matrix = np.array([[2, 0],
+                           [0, 2]])
+
+
