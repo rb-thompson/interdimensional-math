b924b0a4edc2f7988da57cccc9cb82f961597acc
Brandon Thompson <prizecoffeecup@gmail.com>
2025-02-13 11:14:41 -0500

    Combine transformations

--- portal_simulator.py
+++ portal_simulator.py
@@ -6,7 +6,6 @@ object_position = np.array([1, 1])
 
 # Define transformation matrices
 theta = np.pi / 2                   # 'theta': a plane angle
-print(theta)                        #  output: 1.5707963267948966
 
 # Rotation matrix (90 degrees)
 rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)],
@@ -15,5 +14,9 @@ rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)],
 # Scaling matrix (double the size)
 scaling_matrix = np.array([[2, 0],
                            [0, 2]])
+
+# Combine transformations (rotate then scale)
+# @ calculates the product of two matrices
+portal_transform = scaling_matrix @ rotation_matrix
 
 
